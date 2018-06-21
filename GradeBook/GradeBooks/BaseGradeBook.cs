050b4a6526f98e27645d7dfbd4547ccc7acf3439
Phil Jotham <phil.jotham@scanmarqed.com>
2018-06-21 15:45:25 +0100

    Add Support for Weighted Grading
    - Update BaseGradeBook's GetGPA Method

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -109,18 +109,30 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            int GPA;
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    GPA = 4;
+                    break;
                 case 'B':
-                    return 3;
+                    GPA = 3;
+                    break;
                 case 'C':
-                    return 2;
+                    GPA = 2;
+                    break;
                 case 'D':
-                    return 1;
+                    GPA = 1;
+                    break;
+                default:
+                    GPA = 0;
+                    break;
             }
-            return 0;
+            if (IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
+            {
+                GPA += 1;
+            }
+            return GPA;
         }
 
         public virtual void CalculateStatistics()
